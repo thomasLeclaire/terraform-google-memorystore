c8301a177e5d3f7ed2856bf512f2b80aa73b141b
Thomas Leclaire <tleclaire@solocal.com>
2024-04-09 16:39:26 +0200

    update provider version
    Signed-off-by: Thomas Leclaire <tleclaire@solocal.com>

--- modules/backup/versions.tf
+++ modules/backup/versions.tf
@@ -15,11 +15,11 @@
  */
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.3"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0, < 5.0"
+      version = ">= 4.0.0, < 6.0"
     }
   }
 }
